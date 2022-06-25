826434d08377f80fdff70c926978267051bff928
Nikita Antipin <nikprime1@gmail.com>
2022-06-25 17:09:45 +0300

    feat: added logger for end points in project

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan(':method :url :status content-length: :res[content-length] - :response-time ms'));
   app.enableCors();
   app.setGlobalPrefix('api');
 
